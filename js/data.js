4e5a6f2d506a580f1f1a06d188297d924b9e04a5
Tobias Argain <tobiasaargain@gmail.com>
2022-05-25 22:17:53 -0300

    primer commit

---
+++ js/data.js
@@ -0,0 +1,25 @@
+const proyects = [
+
+    {
+        name:"Scala Optical",
+        url:"https://argaintobias.github.io/proyectoFinalDW/",
+        image:"../images/scalaOptical.png"
+    },
+    {
+        name:"Landing Page",
+        url:"https://argaintobias.github.io/landingPage/",
+        image:"../images/landingPage.png"
+    },
+    {
+        name:"E-Commerce",
+        url:"https://argaintobias.github.io/proyectoFinalJS/",
+        image:"../images/e-commerce.png"
+    },
+    {
+        name:"Perla Negra",
+        url:"https://argaintobias.github.io/perlaNegra/",
+        image:"../images/perlaNegra.png"
+    }
+];
+
+technologies = ["HTML5", "CSS (FLEXBOX, GRIDS)", "JS", "BOOSTRAP", "SASS", "GIT"];
---
+++ js/main.js
@@ -0,0 +1,34 @@
+const nodoProyects = document.querySelector(".our-projects");
+const nodoTechnolgies = document.querySelector(".my-knowledge");
+
+const mostrarProyectos = () =>{
+
+    proyects.forEach((proyect)=>{
+        const contenedorInfo = document.createElement("div");
+        contenedorInfo.innerHTML = `<div">
+            <img src="${proyect.image}">
+            <h3>${proyect.name}</h3>
+            <a href="${proyect.url}" target="_blank">View project</a>
+        </div>`;
+        nodoProyects.appendChild(contenedorInfo);
+    })
+}
+
+const mostrarTecnologias = () =>{
+
+
+    technologies.forEach((technology) => {
+        const ulTechnologies = document.createElement("ul");
+        ulTechnologies.innerHTML = `<li>${technology}</li>`;
+        nodoTechnolgies.appendChild(ulTechnologies);
+    })
+
+}
+
+const init = () => {
+
+    mostrarProyectos();
+    mostrarTecnologias();
+}
+
+init();
